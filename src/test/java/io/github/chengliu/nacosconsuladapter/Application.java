235a53f3025ece5e7e9ea646b74098acc634a6b2
liucheng <cheng_liu@kuaidi100.com>
2023-08-09 09:37:17 +0800

    添加测试内容

--- src/test/java/io/github/chengliu/nacosconsuladapter/Application.java
+++ src/test/java/io/github/chengliu/nacosconsuladapter/Application.java
@@ -22,9 +22,13 @@
  */
 package io.github.chengliu.nacosconsuladapter;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @description:
@@ -34,6 +38,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
